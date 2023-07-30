d5886eacb01cf00e964b71f3223e2bbfdea28179
eversometiculous <14371@coderacademy.edu.au>
2023-07-30 11:18:48 +1000

    Comment Controllers

--- controllers/team_thread_controller.py
+++ controllers/team_thread_controller.py
@@ -3,6 +3,8 @@ from init import db
 from models.team_thread import Team_thread, team_thread_schema, team_threads_schema
 from datetime import date
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from models.comment import Comment, comment_schema, comments_schema
+from models.user import User, user_schema, users_schema
 
 
 team_threads_bp = Blueprint('team_threads', __name__, url_prefix='/team_threads')
@@ -34,7 +36,7 @@ def create_team_thread():
         user_id=get_jwt_identity(),
         team_id=get_jwt_identity()
     )
-    # Add that card to the session
+    # Add that team_thread to the session
     db.session.add(team_thread)
     # Commit
     db.session.commit()
@@ -67,3 +69,51 @@ def update_one_team_thread(id):
         return team_thread_schema.dump(team_thread)
     else:
         return { 'error': f'The team thread with id no.{id} does not exist and cannot be updated!'}, 404
+    
+
+# /team_thread/team_thread_id/comments - POST
+
+@team_threads_bp.route('/<int:team_thread_id>/comments', methods=['POST'])
+@jwt_required()
+def create_comment(team_thread_id):
+    body_data = request.get_json()
+    stmt = db.select(Team_thread).filter_by(id=team_thread_id) # select * from team_threads where id = team_thread_id
+    team_thread = db.session.scalar(stmt)
+    if team_thread:
+        comment = Comment(
+            message = body_data.get('message'),
+            date=date.today(),
+            user_id=get_jwt_identity(),
+            team_thread_id=team_thread.id # or team_thread=team_thread
+        )
+
+        db.session.add(comment)
+        db.session.commit()
+        return comment_schema.dump(comment), 201
+    else:
+        return { 'error': f'Team_thread with id no.{id} does not exist and cannot be commented on!'}, 404
+    
+@team_threads_bp.route('/<int:team_thread_id>/comments/<int:comment_id>', methods=['DELETE'])
+@jwt_required()
+def delete_comment(team_thread_id, comment_id):
+    stmt = db.select(Comment).filter_by(id=comment_id)
+    comment = db.session.scalar(stmt)
+    if comment:
+        db.session.delete(comment)
+        db.session.commit()
+        return { 'message': f'Comment {comment.message} deleted successfully!'}
+    else:
+        return { 'error': f'Comment with id no.{comment_id} does not exist'}, 404
+    
+@team_threads_bp.route('/<int:team_thread_id>/comments/<int:comment_id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_comment(team_thread_id, comment_id):
+    body_data = request.get_json()
+    stmt = db.select(Comment).filter_by(id=comment_id)
+    comment = db.session.scalar(stmt)
+    if comment:
+        comment.message = body_data.get('message') or comment.message
+        db.session.commit()
+        return comment_schema.dump(comment)
+    else:
+        return { 'error': f'Comment with id no.{comment_id} does not exist and cannot be editted'}, 404
